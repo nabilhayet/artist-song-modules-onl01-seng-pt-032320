6046f7934ee1a298fe217849a7ab863b409e8152
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-27 01:47:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,19 +21,7 @@ end
     @@songs
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
-  # end
-
   def artist=(artist)
     @artist = artist
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
